4975e4075249c35f043424647de713ff041256e0
Flavio Yukio <flavio_ohphata@hotmail.com>
2026-05-26 18:16:30 -0300

    primeiro commit

---
+++ src/model/entities/CarRental.java
@@ -0,0 +1,65 @@
+package model.entities;
+
+import java.time.LocalDateTime;
+
+public class CarRental {
+	
+	private LocalDateTime start;
+	private LocalDateTime finish;
+	
+	private Vehicle vehicle;
+	private Invoice invoce;
+	
+
+  public CarRental() {	  
+}
+
+  public CarRental(LocalDateTime start, LocalDateTime finish, Vehicle vehicle) {
+	this.start = start;
+	this.finish = finish;
+	this.vehicle = vehicle;
+  }
+
+
+  public LocalDateTime getStart() {
+	return start;
+  }
+
+
+  public void setStart(LocalDateTime start) {
+	this.start = start;
+  }
+
+
+  public LocalDateTime getFinish() {
+	return finish;
+  }
+
+
+  public void setFinish(LocalDateTime finish) {
+	this.finish = finish;
+  }
+
+
+  public Vehicle getVehicle() {
+	return vehicle;
+  }
+
+
+  public void setVehicle(Vehicle vehicle) {
+	this.vehicle = vehicle;
+  }
+
+
+  public Invoice getInvoce() {
+	return invoce;
+  }
+
+
+  public void setInvoce(Invoice invoce) {
+	this.invoce = invoce;
+  }
+  
+}
+
+
